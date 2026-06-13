10754687d4b8040811d8b9e48e2ee22852e2dd48
shofucchi <0904kinazu@gmail.com>
2026-06-13 10:13:22 +0900

    optimize Distinct

---
+++ Sorting/Distinct.swift
@@ -0,0 +1,12 @@
+import Foundation
+import Glibc
+
+// you can write to stdout for debugging purposes, e.g.
+// print("this is a debug message")
+
+public func solution(_ A : inout [Int]) -> Int {
+    // Implement your solution here
+
+    let set = Set(A)
+    return set.count
+}
